eab61118f9f93040c65d091a4639b6c4bb24f1ca
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-02-22 18:37:46 +0200

    Initial commit.

---
+++ src/settings_screen/settings_screen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_MasterVolumeSlider_value_changed(value):
+	Settings.master_volume = value
+
+
+func _on_MusicVolumeSlider_value_changed(value):
+	Settings.music_volume = value
+
+
+func _on_SfxVolumeSlider_value_changed(value):
+	Settings.sfx_volume = value
+
+
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://title_screen/title_screen.tscn")
