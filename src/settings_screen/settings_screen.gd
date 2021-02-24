d820b0d91d7e7058bcb7d5db48ef3d429096169b
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-02-24 19:31:41 +0200

    Add audio buses. Add speech volume setting.

--- src/settings_screen/settings_screen.gd
+++ src/settings_screen/settings_screen.gd
@@ -13,5 +13,9 @@ func _on_SfxVolumeSlider_value_changed(value):
 	Settings.sfx_volume = value
 
 
+func _on_SpeechVolumeSlider_value_changed(value):
+	Settings.speech_volume = value
+
+
 func _on_BackButton_pressed():
 	get_tree().change_scene("res://title_screen/title_screen.tscn")
--- src/util/settings.gd
+++ src/util/settings.gd
@@ -9,10 +9,18 @@ const MIN_VOLUME = 0.0
 var master_volume: float = DEFAULT_VOLUME setget set_master_volume
 var sfx_volume:float = DEFAULT_VOLUME setget set_sfx_volume
 var music_volume:float = DEFAULT_VOLUME setget set_music_volume
+var speech_volume:float = DEFAULT_VOLUME setget set_speech_volume
 
 var _master_bus_idx = -1
 var _sfx_bus_idx = -1
 var _music_bus_idx = -1
+var _speech_bus_idx = -1
+
+func _ready():
+	_master_bus_idx = AudioServer.get_bus_index("Master")
+	_music_bus_idx = AudioServer.get_bus_index("Music")
+	_sfx_bus_idx = AudioServer.get_bus_index("Sfx")
+	_speech_bus_idx = AudioServer.get_bus_index("Speech")
 
 
 func set_master_volume(value: float):
@@ -28,6 +36,11 @@ func set_sfx_volume(value: float):
 func set_music_volume(value: float):
 	music_volume = clamp(value, MIN_VOLUME, MAX_VOLUME)
 	_set_volume(_music_bus_idx, value)
+
+
+func set_speech_volume(value: float):
+	speech_volume = clamp(value, MIN_VOLUME, MAX_VOLUME)
+	_set_volume(_speech_bus_idx, value)
 
 
 func _set_volume(bus_idx: int, volume: float):
